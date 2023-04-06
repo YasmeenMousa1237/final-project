c60ab98d95d7541575be39b0322679cae2422228
ibrmoh <ibrahim1412nn@gmail.com>
2023-04-06 15:26:52 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function(){
+    $name ='Yasmeen';
+  //  return view ('about',[
+      //  'name'=> $name
+  //  ]) ;
+  return view('about', compact('name')) ;
+}) ;
+
+Route::get('tasks',function(){
+    $tasks=[
+        'first-task' =>'Task1',
+        'second-task' => 'Task2' ,
+        'third-task' => 'Task3'
+    ];
+return view('tasks',compact('tasks')) ;
+});
+
+Route::get('show/{id}' ,function($id){
+    $tasks=[
+        'first-task' =>'Task1',
+        'second-task' => 'Task2' ,
+        'third-task' => 'Task3'
+    ];
+    $task= $tasks[$id] ;
+return view('show' , compact('task')) ;
+});
+
+
